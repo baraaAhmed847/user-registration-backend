df97e63c91177a6a66edcb07835bc880cfde0737
baraaAhmed847 <baraa.a.dev@gmail.com>
2026-07-02 11:32:00 +0300

    fix CORS headers

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-
 from dotenv import load_dotenv
 import os
 import secrets
@@ -23,10 +22,10 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
-# السماح بالمنافذ المحلية ومنافذ ريلواي مستقبلاً
-from flask_cors import CORS, cross_origin
+CORS(app, resources={r"/*": {"origins": "*"}},
+     allow_headers=["Content-Type", "Authorization"],
+     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 
-CORS(app)
 reset_tokens = {}
 
 class User(db.Model):
@@ -246,6 +245,5 @@ def reset_password():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    # لتسهيل التشغيل على ريلواي يفضل قراءة البورت من البيئة
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
